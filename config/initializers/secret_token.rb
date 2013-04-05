80787e12048a46eb4d888a18768f7107614f730f
Hiroki Matsue <hiroki.matsue@gmail.com>
2013-04-06 06:52:16 +0900

    Initialize for Rails4 with MySQL.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Oresabre::Application.config.secret_key_base = '9b3db4d55e688057eb9685c2dc677da435115a9888f6bc3b85648eb2cd0bad10900cd80a74b4a7d41cb0e407a7650869a63e2f0ffa6bfc195ce833380d32af45'
